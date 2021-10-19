6d329a94fefdf4f051614bf3098a269b7bd1ec71
George <cavazosgeorge@yahoo.com>
2021-10-19 09:03:13 -0400

    create registration form

---
+++ frontend/src/registrationForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RegistrationForm.css'
+
+export default function RegisterForm(props) {
+    const {
+    values,
+    submit,
+    change,
+    errors, 
+    } = props
