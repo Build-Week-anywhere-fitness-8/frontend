1fe5f121ced9a31f775b0e98c274d009d6ef6e66
IrvinA <arevalosirvin@gmail.com>
2021-10-19 14:20:23 -0700

    Added Private routes, Class form, class list, and class display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import About from './components/About'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import PrivateRoute from './components/PrivateRoute';
+import Logout from './components/Logout';
+import ClassList from './components/ClassList';
+import ClassForm from './components/AddClassForm';
 
 import './App.css';
 
@@ -15,13 +20,26 @@ import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, Nav
 
 function App() {
 
-
+  const [classList, setClassList] = useState([])
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [dropdownLocations, setDropdownLocations] = useState(false);
 
 
   const toggleClasses = () => setDropdownOpen(!dropdownOpen);
   const toggleLocations = () => setDropdownLocations(!dropdownLocations);
+
+  useEffect(() => {
+    axios.get('http://anytimefitness.herokuapp.com/api/classes')
+        .then(res => {
+            setClassList([
+                ...classList,
+                res.data
+            ]);
+        })
+        .catch(err => {
+            console.error(err);
+        })
+}, [])
 
   return (
 
@@ -43,15 +61,16 @@ function App() {
             <NavItem>
               <NavLink href="#"><Link className='item' to='/register'>Register</Link></NavLink>
             </NavItem>
+            <NavItem>
+              <NavLink href="#"><Link className='item' to='/logout'>Logout</Link></NavLink>
+            </NavItem>
 
             <Dropdown nav isOpen={dropdownOpen} toggle={toggleClasses}>
               <DropdownToggle nav caret>
                 <span className='item'>Classes</span>
               </DropdownToggle>
               <DropdownMenu>
-                <DropdownItem>Zumba</DropdownItem>
-                <DropdownItem>Cycling</DropdownItem>
-                <DropdownItem>Cross Fit</DropdownItem>
+                {classList.map(c => {<DropdownItem>{c.class_name}</DropdownItem>})}
               </DropdownMenu>
             </Dropdown>
 
@@ -74,9 +93,12 @@ function App() {
         </header>
 
         <Switch>
+          <PrivateRoute path='/logout' component={Logout} />
+          <PrivateRoute path='/add-class' component={ClassForm}/>
+          <Route path='classlist' component={ClassList}/>
           <Route path='/about' component={About} />
           <Route path='/register' component={Register} />
-          <Route path='/login' component={Login} />
+          <Route exact path='/login' component={Login} />
           <Route path='/' component={Login} />
         </Switch>
       </div>
