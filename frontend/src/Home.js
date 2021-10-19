50b72adc447dd2ce396f7e638686920659b3fbae
George <cavazosgeorge@yahoo.com>
2021-10-19 10:05:29 -0400

    updated register endpoint

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import RegisterForm from "./registrationForm";
 
 function Home() {
   return (
     <div>
       <h3>Welcome Build Week Crew</h3>
-      <RegisterForm />
     </div>
   );
 }
