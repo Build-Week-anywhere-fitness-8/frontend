cb0e72f93444e0f056666f3f328cb0444c26e234
cc <votaryofturms@icloud.com>
2021-10-20 19:24:55 -0400

    created states and functions for onboard modal

--- frontend/src/components/OnboardModal.js
+++ frontend/src/components/OnboardModal.js
@@ -3,6 +3,24 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const OnboardModal = (props) => {
 
+    const [modal, setModal] = useState(false)
+    const [nestedModal, setNestModal] = useState(false)
+    const [closeAll, setCloseAll] = useState(false)
+
+    const toggle = () => setModal(!modal)
+    const toggleNested = () => {
+        setNestModal(!nestedModal)
+        setCloseAll(false)
+    }
+
+    const toggleAll = () => {
+        setNestedModal(!nestedModal)
+        setCloseAll(true)
+    }
+
+    return (
+        null
+    )
 
 }
 
