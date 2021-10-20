41b59e2502787f8bffc4707a0231a90a16d02dfa
cc <votaryofturms@icloud.com>
2021-10-20 19:03:21 -0400

    created onboard modal component

---
+++ frontend/src/components/OnboardModal.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const OnboardModal = (props) => {
+
+
+}
+
+export default OnboardModal
