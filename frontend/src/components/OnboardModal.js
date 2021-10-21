30307ff76d917d076ecb8978eb30aa4378acfc6b
cc <votaryofturms@icloud.com>
2021-10-21 09:41:33 -0400

    test commit

--- frontend/src/components/OnboardModal.js
+++ frontend/src/components/OnboardModal.js
@@ -3,42 +3,41 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const OnboardModal = (props) => {
 
-    const ModalExample = (props) => {
-        const {
-            buttonLabel,
-            className
-        } = props;
+    const {
+        buttonLabel,
+        className
+    } = props;
 
-        const [modal, setModal] = useState(true)
-        const [nestedModal, setNestModal] = useState(false)
-        const [closeAll, setCloseAll] = useState(false)
+    const [modal, setModal] = useState(true)
+    const [nestedModal, setNestModal] = useState(false)
+    const [closeAll, setCloseAll] = useState(false)
 
-        const toggle = () => setModal(!modal)
-        const toggleNested = () => {
-            setNestModal(!nestedModal)
-            setCloseAll(false)
-        }
-
-        const toggleAll = () => {
-            setNestedModal(!nestedModal)
-            setCloseAll(true)
-        }
-
-        return (
-            <>
-                <div>
-                    <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
-                    <Modal>
-                        <ModalHeader>Welcome!</ModalHeader>
-                        <ModalBody>If you would like to learn more about how to use our app and out classes click below, otherwise have fun!
-                            <br />
-                            <Button></Button>
-                        </ModalBody>
-                    </Modal>
-                </div>
-            </>
-        )
-
+    const toggle = () => setModal(!modal)
+    const toggleNested = () => {
+        setNestModal(!nestedModal)
+        setCloseAll(false)
     }
 
-    export default OnboardModal
+    const toggleAll = () => {
+        setNestedModal(!nestedModal)
+        setCloseAll(true)
+    }
+
+    return (
+        <>
+            <div>
+                <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+                <Modal>
+                    <ModalHeader>Welcome!</ModalHeader>
+                    <ModalBody>If you would like to learn more about how to use our app and out classes click below, otherwise have fun!
+                        <br />
+                        <Button></Button>
+                    </ModalBody>
+                </Modal>
+            </div>
+        </>
+    )
+
+}
+
+export default OnboardModal
