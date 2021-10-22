105c32b38ec6645d00ca60ce5015d460b178bcd5
cc <votaryofturms@icloud.com>
2021-10-22 04:23:09 -0400

    added further class sign up form functionality

--- frontend/src/ClassSignup.js
+++ frontend/src/ClassSignup.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, FormGroup, Label, Input, } from 'reactstrap';
 
 
 const ClassSignup = (props) => {
 
     const { classes } = props
-    console.log(classes)
+
     let classList = classes.length > 0
         && classes.map((item, index) => {
             return <option key={index} value={item.class_id}>{item.class_name}</option>
@@ -16,21 +16,88 @@ const ClassSignup = (props) => {
             return <option key={index} value={item.class_id}>{item.class_location}</option>
         })
 
-    const classType = null
+    const classesInput = document.getElementById('class-select')
 
+    const [formValue, setFormValue] = useState('')
 
+    const inputChange = (name, value) => {
+        setFormValue({
+            ...formValue,
+            [name]: value
+        })
+    }
+
+    const onChange = event => {
+        const { name, value } = event.target;
+        inputChange(name, value);
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+    }
+
+    const classInfo = classes.map((item, index) => {
+        if (item.class_id === Number(formValue['class-select'])) {
+            return (<>
+                <FormGroup>
+                    <Label>Type:&nbsp;
+                        <span>
+                            {item.class_type}
+                        </span>
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Duration:&nbsp;
+                        <span>
+                            {item.class_duration} Minutes
+                        </span>
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Intensity:&nbsp;
+                        <span>
+                            {item.class_intensity}
+                        </span>
+                    </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label>Max Class Size:&nbsp;
+                        <span>
+                            {item.class_max_attendees}
+                        </span>
+                    </Label>
+                </FormGroup>
+            </>
+            )
+        }
+    })
+
+    // console.log(classInfo)
+    // console.log(formValue)
+    // console.log(classes)
+    // console.log(classesInput)
     return (
         <>
-            <Form>
+            <Form
+                onSubmit={onSubmit}
+                style={{
+                    border: 'solid 1px grey',
+                    borderRadius: '.5rem',
+                    margin: '3rem',
+                    marginLeft: '25%',
+                    marginRight: '25%',
+                    padding: '2rem',
+                    backgroundColor: 'lightgray'
+                }}>
                 <h2>Class Sign-up</h2>
-                <FormGroup style={{ marginLeft: '40%', marginRight: '40%' }}>
+                <FormGroup className='my-1' style={{ marginLeft: '35%', marginRight: '35%' }}>
                     <Label>Class</Label>
-                    <Input style={{ textAlign: 'center' }} type='select' name='class-select'>
+                    <Input onChange={onChange} value={formValue.value} style={{ textAlign: 'center' }} type='select' name='class-select' id='class-select'>
                         <option>Select a class</option>
                         {classList}
                     </Input>
                 </FormGroup>
-                <FormGroup style={{ marginLeft: '40%', marginRight: '40%' }}>
+                <FormGroup className='my-1' style={{ marginLeft: '35%', marginRight: '35%' }}>
                     <Label>Location</Label>
                     <Input style={{ textAlign: 'center' }} type='select' name='location-select'>
                         <option>Select a location</option>
@@ -38,7 +105,10 @@ const ClassSignup = (props) => {
                     </Input>
                 </FormGroup>
                 <FormGroup>
-                    { }
+                    {classInfo}
+                </FormGroup>
+                <FormGroup className='mt-3'>
+                    <Button>Confirm</Button>
                 </FormGroup>
             </Form>
         </>
